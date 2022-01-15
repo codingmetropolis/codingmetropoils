10331f19df28d481a6496ef987e86f5a3a8a7e8b
HEGADE <subramanyashegade@gmail.com>
2022-01-15 11:11:54 +0530

    made gphQl queries in different file

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,15 +1,14 @@
-import React, { useState, useEffect } from "react";
 
 import Link from "next/link";
 import { getCategories } from "../services";
+const navLinks=[
+  { name: "blogs", url: "/blogs" },
+  { name: "projects", url: "/#" },
+  { name: "services", url: "/services" },
+  { name: "Home", url: "/" },
 
+]
 const Header = () => {
-  const [navLinks, setNavLinks] = useState([
-    { name: "blogs", url: "/blogs" },
-    { name: "projects", url: "/#" },
-    { name: "services", url: "/#" },
-  ]);
-
   return (
     <div className="container mx-auto px-10 mb-8">
       <div className="border-b w-full inline-block border-blue-400 py-8">
---
+++ pages/services/index.jsx
@@ -0,0 +1,9 @@
+const index = () => {
+    return (
+        <>
+          <h1>Welcome to services</h1>  
+        </>
+    )
+}
+
+export default index
