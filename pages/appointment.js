9e1f4e64e8689270777a50a68a4edec3df453f75
vignesh-cloud-prog <vigneshun80@gmail.com>
2024-07-15 14:48:49 +0530

    fix: script problem

--- pages/appointment.js
+++ pages/appointment.js
@@ -1,13 +1,8 @@
-'use client';
-
+import Script from 'next/script';
 import React, { useEffect } from 'react';
 
 export default function Appointment() {
-    useEffect(() => {
-        window.onload = () =>
-            
-        window.jotformEmbedHandler("iframe[id='JotFormIFrame-241935017318455']", "https://form.jotform.com/");
-    }, []);
+
 
     return (
         <div>
@@ -22,7 +17,14 @@ export default function Appointment() {
                 style={{ minWidth: "100%", maxWidth: "100%", height: "539px", border: "none" }}
                 scrolling="no"
             ></iframe>
-            <script src="https://cdn.jotfor.ms/s/umd/latest/for-form-embed-handler.js"></script>
+            <Script src="https://cdn.jotfor.ms/s/umd/latest/for-form-embed-handler.js" strategy="beforeInteractive" />
+
+            <Script>
+                {`
+        window.jotformEmbedHandler("iframe[id='JotFormIFrame-241935017318455']", "https://form.jotform.com/");
+        `}
+            </Script>
+
         </div>
     );
 }
