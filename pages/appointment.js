8e6f31a16fda42cc4d78f6f4a14f998b9eafd11f
vignesh-cloud-prog <vigneshun80@gmail.com>
2024-07-18 12:46:12 +0530

    verify trust piolot

--- pages/appointment.js
+++ pages/appointment.js
@@ -1,5 +1,5 @@
 import Script from 'next/script';
-import React, { useEffect } from 'react';
+import React from 'react';
 
 export default function Appointment() {
 
@@ -10,14 +10,16 @@ export default function Appointment() {
                 id="JotFormIFrame-241935017318455"
                 title="CM Appointment Request Form"
                 onLoad={() => window.parent.scrollTo(0, 0)}
-                allowtransparency="true"
                 allow="geolocation; microphone; camera; fullscreen"
                 src="https://form.jotform.com/241935017318455"
-                frameBorder="0"
                 style={{ minWidth: "100%", maxWidth: "100%", height: "539px", border: "none" }}
-                scrolling="no"
+              
             ></iframe>
-            <Script src="https://cdn.jotfor.ms/s/umd/latest/for-form-embed-handler.js" strategy="beforeInteractive" />
+            <Script src="https://cdn.jotfor.ms/s/umd/latest/for-form-embed-handler.js" strategy="beforeInteractive" onLoad={
+                () => {
+                    `window.jotformEmbedHandler("iframe[id='JotFormIFrame-241935017318455']", "https://form.jotform.com/");`
+                }
+            } />
 
             <Script>
                 {`
